7ec8543f077b9abd3f71731929f9745eede2baf2
hiphoplsy <hiphoplsy@naver.com>
2020-06-09 16:51:14 +0900

    ADD: typo

--- front/config/config.js
+++ front/config/config.js
@@ -1,3 +1,3 @@
-export const backUrl = process.env.NODE_ENV = 'production' ? 'http://api.marshio.com' : 'http://localhost:3065/';
+export const backUrl = process.env.NODE_ENV === 'production' ? 'http://api.marshio.com' : 'http://localhost:3065/';
 
 export { backUrl };
