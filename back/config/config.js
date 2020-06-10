d81302c20be2e89124376dd7c8ce041cef65143c
hiphoplsy <hiphoplsy@naver.com>
2020-06-10 11:33:51 +0900

    ADD: typo

--- back/config/config.js
+++ back/config/config.js
@@ -3,28 +3,25 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": process.env.DB_PASSWORD,
-    "database": "react-nodebird",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+  development: {
+    username: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'react-nodebird',
+    host: '127.0.0.1',
+    dialect: 'mysql',
   },
-  "test": {
-    "username": "root",
-    "password": process.env.DB_PASSWORD,
-    "database": "react-nodebird",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+  test: {
+    username: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'react-nodebird',
+    host: '127.0.0.1',
+    dialect: 'mysql',
   },
-  "production": {
-    "username": "root",
-    "password": process.env.DB_PASSWORD,
-    "database": "react-nodebird",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  }
+  production: {
+    username: 'root',
+    password: process.env.DB_PASSWORD,
+    database: 'react-nodebird',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
 }
