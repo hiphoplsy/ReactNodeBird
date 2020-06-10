f41a4d24828d68b73b53cb9a723d0d23df092e19
hiphoplsy <hiphoplsy@naver.com>
2020-06-10 15:03:36 +0900

    Update: typo

--- front/components/styles/imagesZoomStyle.js
+++ front/components/styles/imagesZoomStyle.js
@@ -43,10 +43,10 @@ export const Indicator = styled.div`
   text-align: center;
 
   & > div {
-    width: 75; 
-    height: 30; 
+    width: 75px; 
+    height: 30px; 
     line-height: 30px; 
-    border-radius: 15; 
+    border-radius: 15px; 
     background: #313131; 
     display: inline-block; 
     text-align: center; 
